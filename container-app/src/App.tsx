70628aaae01a5bf2f04adae4d67c0bd1c0f11b1c
Rahul Jaiswal <jaiswal.rahul20m96@gmail.com>
2023-06-15 18:12:55 +0530

    commit

--- container-app/src/App.tsx
+++ container-app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@itwin/itwinui-react';
 
 // import RemoteComponentType from "remote/RemoteComponent";
 
@@ -8,6 +9,7 @@ const RemoteComponent = React.lazy(() => import('remote/RemoteComponent'));
 function App() {
   return (
     <div className="App">
+      <Button styleType='cta'>Itwin UI Button from Container App</Button>
       <RemoteComponent />
     </div>
   );
